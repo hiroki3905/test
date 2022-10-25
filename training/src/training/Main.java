d6795c368442cf26fefff07bfe19154579cc4749
gteng <gteng@LAPTOP-NLAQD9L8>
2022-10-26 04:57:01 +0900

    挨拶追加　#2

--- training/src/training/Main.java
+++ training/src/training/Main.java
@@ -6,8 +6,12 @@ public class Main {
 		// TODO 自動生成されたメソッド・スタブ
 		Threadrun t1 = new Threadrun();
 		Threadrun t2 = new Threadrun();
+		Threadrun t3 = new Threadrun();
+		Threadrun t4 = new Threadrun();
         t1.start();
         t2.start();
+        t3.start();
+        t4.start();
 	}
 
 }
--- training/src/training/Threadrun.java
+++ training/src/training/Threadrun.java
@@ -9,12 +9,16 @@ class Threadrun extends Thread{
 		Random rand = new Random();
 		Date date = new Date();
 		SimpleDateFormat sdf = new SimpleDateFormat("kk/mm/ss");
+		
+		
 		int x = rand.nextInt(10);
-        System.out.println(getName()+"が起動しました。"+x+"後に停止します。"+sdf.format(date));
+        System.out.println(getName()+"が起動しました。"+x+"後に停止します。");
+        
         try {
             Thread.sleep(1000 * x);
         } catch (InterruptedException e) {
         }
+        
         System.out.println(getName()+"が終了すました");
     }
 
