9c2e023e250724c3e7517b001d6d189410b2ed23
gteng <gteng@LAPTOP-NLAQD9L8>
2022-10-26 04:58:25 +0900

    挨拶追加　完成

--- training/src/training/Main.java
+++ training/src/training/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("こんにちは！");
 		Threadrun t1 = new Threadrun();
 		Threadrun t2 = new Threadrun();
 		Threadrun t3 = new Threadrun();
