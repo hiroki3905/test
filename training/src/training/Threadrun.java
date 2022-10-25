fe7659ff95d581e60229855c515ba6f553008d9a
gteng <gteng@LAPTOP-NLAQD9L8>
2022-10-26 04:20:37 +0900

    スレッド起動時間追加

--- training/src/training/Threadrun.java
+++ training/src/training/Threadrun.java
@@ -1,12 +1,15 @@
 package training;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Random;
 
 class Threadrun extends Thread{
 	public void run() {
 		Random rand = new Random();
-		rand.nextInt(10);
-        System.out.println(getName()+"が起動しました。"+rand.nextInt(10)+"後に停止します。");
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("kk/mm/ss");
+        System.out.println(getName()+"が起動しました。"+rand.nextInt(10)+"後に停止します。"+sdf.format(date));
     }
 
 }
